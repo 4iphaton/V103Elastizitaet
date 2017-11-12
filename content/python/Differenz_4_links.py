f7b082d7bf72e5d2472f55383ce3db99894246c6
Jan Ellbracht <jan.ellbracht@tu-dortmund.de>
2017-11-12 22:30:04 +0100

    Differenzen

--- content/python/Differenz_4_links.py
+++ content/python/Differenz_4_links.py
@@ -1,12 +1,10 @@
-# Nur lokal ausführen. Datei ist nicht in der Makefile
-
 import numpy as np
 import scipy.constants as const
 from uncertainties import ufloat
 from uncertainties import unumpy as unp
 import math
 
-x, h_kein , h_Gew = np.genfromtxt('../../content/values/Messwerte_2.txt',unpack=True)
+x, h_kein , h_Gew = np.genfromtxt('../../content/values/Messwerte_4_links.txt',unpack=True)
 
 print(x,h_kein,h_Gew)
 
