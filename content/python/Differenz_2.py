aaaf9c91948e846e62079f277abc16c7720931fb
Jan Ellbracht <jan.ellbracht@tu-dortmund.de>
2017-11-12 21:41:32 +0100

    push it

---
+++ content/python/Differenz_2.py
@@ -0,0 +1,17 @@
+# Nur lokal ausführen. Datei ist nicht in der Makefile
+
+import numpy as np
+import scipy.constants as const
+from uncertainties import ufloat
+from uncertainties import unumpy as unp
+import math
+
+x, h_kein , h_Gew = np.genfromtxt('../../content/values/Messwerte_2.txt',unpack=True)
+
+print(x,h_kein,h_Gew)
+
+#n = len(h_kein) # Alle Arrays haben dieselbe laenge
+
+#berechnen der Minima bzw Maxima
+D = h_kein - h_Gew
+print (h_kein)
