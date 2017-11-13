1ab7ddd81a1774dcffd18520338bf9271c31875c
Max Krebs <maxkrebs1@gmail.com>
2017-11-13 20:08:25 +0100

    added 2 files

---
+++ content/python/plot3.py
@@ -0,0 +1,7 @@
+import numpy as np
+from scipy.optimize import curve_fit
+from scipy.signal import find_peaks_cwt
+import matplotlib.pyplot as plt
+from uncertainties import ufloat
+
+a, b, c = np.genfromtxt('content/values/Messwerte_4_links.txt', unpack=True)
