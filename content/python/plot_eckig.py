be23dce420fbef6954fcb3a2e479428ba30a20b5
Jan Ellbracht <jan.ellbracht@tu-dortmund.de>
2017-11-13 17:04:31 +0100

    Stab_eckig

--- content/python/plot_eckig.py
+++ content/python/plot_eckig.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 
 a, b, c = np.genfromtxt('content/values/Messwerte_2.txt', unpack=True)
 # = np.genfromtxt('../../content/values/Differenz_2.txt', unpack=True)
+a= a*0.1
 d= b-c
 
-F=9.81*1190.7
+F=9.81*1193.2
 E_1=(49*0.01*(b*0.01)**2-(b*0.01)**3/3)*10**(-6)
 #print(E_1)
 
@@ -27,8 +28,8 @@ print(parameters, np.sqrt(np.diag(pcov)), sep='\n')
 t= np.linspace(0,80000,1000)
 
 plt.plot(t, f(t, *parameters), 'g-', label='Fit')
-#plt.plot(E, b, 'rx', label='Biegung ohne Gewicht')
-#plt.plot(E, c, 'bx', label='Biegung mit Gewicht')
+plt.plot(E, b, 'rx', label='Biegung ohne Gewicht')
+plt.plot(E, c, 'bx', label='Biegung mit Gewicht')
 plt.plot(E, d, 'gx', label='$ D(x) $')
 #plt.errorbar(i, v ,xerr=2*e_i ,yerr=2*e_v, fmt='rx', label='U gegen I')
 plt.xlabel(r'$ Lx^2 - \frac{x^3}{3}$')
