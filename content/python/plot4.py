bcbce735e146f3f66c59a8aed1c2762018b29782
Max Krebs <maxkrebs1@gmail.com>
2017-11-13 20:10:42 +0100

    changed 2 files

--- content/python/plot4.py
+++ content/python/plot4.py
@@ -5,3 +5,10 @@ import matplotlib.pyplot as plt
 from uncertainties import ufloat
 
 a, b, c = np.genfromtxt('content/values/Messwerte_4_rechts.txt', unpack=True)
+a/=100
+b/=1000
+c/=1000
+
+d= b-c
+F=9.81*4.6937
+L=0.565
