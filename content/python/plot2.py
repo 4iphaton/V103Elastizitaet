8ed36075664889b44afb164c394df41afc192c00
Max Krebs <maxkrebs1@gmail.com>
2017-11-13 17:53:08 +0100

    updated plot2.py

--- content/python/plot2.py
+++ content/python/plot2.py
@@ -2,16 +2,16 @@ import numpy as np
 from scipy.optimize import curve_fit
 from scipy.signal import find_peaks_cwt
 import matplotlib.pyplot as plt
+import scipy.constants as const
 
 a, b, c = np.genfromtxt('content/values/Messwerte_3.txt', unpack=True)
+a/=100
+b/=1000
+c/=1000
+
 d= b-c
-
 F=9.81*533.9
-L=49.3
-E_1=(49.3*0.01*(a*0.01)**2-(a*0.01)**3/3)*10**(-6)
-
-s_1=np.mean(E_1)
-
+L=0.493
 
 
 E=L*a**2-a**3/3
@@ -22,15 +22,15 @@ def f(x, y, b):
 
 parameters, pcov = curve_fit(f, E, d)
 
-t= np.linspace(0,80000,1000)
+t= np.linspace(0,E[len(E)-1],5000)
 
 plt.plot(t, f(t, *parameters), 'g-', label='Fit')
 plt.plot(E, b, 'rx', label='Biegung ohne Gewicht')
 plt.plot(E, c, 'bx', label='Biegung mit Gewicht')
 plt.plot(E, d, 'gx', label='$ D(x) $')
 #plt.errorbar(i, v ,xerr=2*e_i ,yerr=2*e_v, fmt='rx', label='U gegen I')
-plt.xlabel(r'$ Lx^2 - \frac{x^3}{3}$')
-plt.ylabel(r'Auslenkung in $ 10 \mu m$')
+plt.xlabel(r'$ Lx^2 - \frac{x^3}{3}$ / $\text{m}^3$')
+plt.ylabel(r'Auslenkung in $ m$')
 plt.legend(loc='best')
 
 plt.tight_layout()
