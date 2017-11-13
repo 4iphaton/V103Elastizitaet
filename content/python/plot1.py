4b19d7f887873ffd52c71cf77c557bac418fb651
Jan Ellbracht <jan.ellbracht@tu-dortmund.de>
2017-11-13 16:51:00 +0100

    Plot Anpassung

--- content/python/plot1.py
+++ content/python/plot1.py
@@ -3,9 +3,9 @@ from scipy.optimize import curve_fit
 from scipy.signal import find_peaks_cwt
 import matplotlib.pyplot as plt
 
-k, b, c = np.genfromtxt('content/values/Messwerte_2.txt', unpack=True)
+a, b, c = np.genfromtxt('content/values/Messwerte_2.txt', unpack=True)
 # = np.genfromtxt('../../content/values/Differenz_2.txt', unpack=True)
-d= b+c
+d= b-c
 
 F=9.81*1190.7
 E_1=(49*0.01*(b*0.01)**2-(b*0.01)**3/3)*10**(-6)
@@ -27,8 +27,8 @@ print(parameters, np.sqrt(np.diag(pcov)), sep='\n')
 t= np.linspace(0,80000,1000)
 
 plt.plot(t, f(t, *parameters), 'g-', label='Fit')
-plt.plot(E, b, 'rx', label='Biegung ohne Gewicht')
-plt.plot(E, c, 'bx', label='Biegung mit Gewicht')
+#plt.plot(E, b, 'rx', label='Biegung ohne Gewicht')
+#plt.plot(E, c, 'bx', label='Biegung mit Gewicht')
 plt.plot(E, d, 'gx', label='$ D(x) $')
 #plt.errorbar(i, v ,xerr=2*e_i ,yerr=2*e_v, fmt='rx', label='U gegen I')
 plt.xlabel(r'$ Lx^2 - \frac{x^3}{3}$')
