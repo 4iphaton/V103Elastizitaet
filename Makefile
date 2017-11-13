ca45da6eec3615242e13eb6e4582ae54e39173d2
Max Krebs <maxkrebs1@gmail.com>
2017-11-13 20:51:05 +0100

    changed plot3

--- Makefile
+++ Makefile
@@ -15,8 +15,10 @@ build/plot1.pdf: content/python/plot1.py matplotlibrc header-matplotlib.tex | bu
 	TEXINPUTS="$(call translate,$(pwd):)" python content/python/plot1.py
 build/plot2.pdf: content/python/plot2.py matplotlibrc header-matplotlib.tex | build
 	TEXINPUTS="$(call translate,$(pwd):)" python content/python/plot2.py
+build/plot3.pdf: content/python/plot3.py matplotlibrc header-matplotlib.tex | build
+	TEXINPUTS="$(call translate,$(pwd):)" python content/python/plot3.py
 # hier weitere Abhängigkeiten für build/main.pdf deklarieren:
-build/main.pdf: build/plot1.pdf build/plot2.pdf
+build/main.pdf: build/plot1.pdf build/plot2.pdf build/plot3.pdf
 
 build/main.pdf: FORCE | build
 	  TEXINPUTS="$(call translate,build:)" \
--- content/python/plot3.py
+++ content/python/plot3.py
@@ -3,8 +3,9 @@ from scipy.optimize import curve_fit
 from scipy.signal import find_peaks_cwt
 import matplotlib.pyplot as plt
 from uncertainties import ufloat
+import scipy.constants as const
 
-a, b, c = np.genfromtxt('content/values/Messwerte_4_links.txt', unpack=True)
+a, b, c = np.genfromtxt('content/values/Messwerte_4_rechts.txt', unpack=True)
 a/=100
 b/=1000
 c/=1000
@@ -12,3 +13,35 @@ c/=1000
 d= b-c
 F=9.81*4.6937
 L=0.565
+
+E=3*(L**2)*a-4*(a**3)
+print(a[0],a[len(a)-1])
+print(E[0],E[len(E)-1])
+def f(x, y, b):
+    return x*b+y
+
+parameters, pcov = curve_fit(f, E, d)
+errors = np.sqrt(np.diag(pcov))
+print(parameters[0], errors[0])
+m = ufloat(parameters[0], errors[0])
+print('Steigung m = {0:.8f}'.format(m),'1/(m^2)')
+t= np.linspace(E[0],E[len(E)-1],5000)
+
+plt.plot(t, f(t, *parameters), 'g-', label='Fit')
+plt.plot(E, b, 'rx', label='ohne Gewicht')
+plt.plot(E, c, 'bx', label='mit Gewicht')
+plt.plot(E, d, 'gx', label='$ D(x) $')
+#plt.errorbar(i, v ,xerr=2*e_i ,yerr=2*e_v, fmt='rx', label='U gegen I')
+plt.xlabel(r'$ 3L^2x - 4x^3$ / $\text{m}^3$')
+plt.ylabel(r'Auslenkung in $ m$')
+plt.legend(loc='best')
+
+plt.tight_layout()
+
+plt.savefig('build/plot3.pdf')
+I = const.pi * (0.005**4) / 2
+print(I)
+EM = F*m/(48*I)
+
+print("Elastizitätmodul: {0:.2f}".format(EM), "N/m^2")
+print("-------------------------------")
